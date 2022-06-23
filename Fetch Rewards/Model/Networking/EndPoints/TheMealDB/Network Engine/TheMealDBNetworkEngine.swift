963bba342cc4103d3aae8189ace623bd9615284a
Amogh Joshi <amogh@craftedthoughts.com>
2022-06-22 23:56:07 -0400

    getMeals is making network request without async code so I'll fix that now :)

--- Fetch Rewards/Model/Networking/EndPoints/TheMealDB/Network Engine/TheMealDBNetworkEngine.swift
+++ Fetch Rewards/Model/Networking/EndPoints/TheMealDB/Network Engine/TheMealDBNetworkEngine.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 //TheMealDBNetworkEngine is responsible for putting everything together. It uses the end point to construct URL and makes network requests using NetworkModel using NetworkResponse. It also happens to confirm to MealsStorage protocol and hence, currently, provides storage for Model Controllers
 struct TheMealDBNetworkEngine: MealsStorage {
@@ -17,6 +18,7 @@ struct TheMealDBNetworkEngine: MealsStorage {
     }
     
     func getMeals(for category: MealCategory) -> [Meal] {
+        var meals = [Meal]()
         switch category {
         case .dessert:
             let endPoint = EndPoints.TheMealDB.getMeals(category: .dessert)
@@ -25,10 +27,16 @@ struct TheMealDBNetworkEngine: MealsStorage {
             typealias TheMealDBResponse = TheMealDBNetworkResponse<[MealNetworkResponse]>?
             
             networkModel.makeRequest(at: url) { (response:TheMealDBResponse, error) in
-                print(response)
+                for responseMeal in response!.meals {
+                    #warning("get image from the url")
+                    let image = UIImage(systemName: "tornado")!
+                    
+                    let meal = Meal(name: responseMeal.name, image: image, id: Int(responseMeal.id)!)
+                    meals.append(meal)
+                }
             }
         }
         
-        return []
+        return meals
     }
 }
