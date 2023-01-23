19a1da3fdf6b1e607f2446daee5a03721a1fb479
Amogh Joshi <amogh@craftedthoughts.com>
2023-01-23 15:29:42 -0800

    got the imageview to work

--- Fetch Rewards/View/Meals/MealTableViewCell.swift
+++ Fetch Rewards/View/Meals/MealTableViewCell.swift
@@ -13,12 +13,24 @@ class MealTableViewCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.layer.cornerRadius = 10
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         return imageView
+    }()
+    
+    private lazy var mealName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.textColor = .label
+        return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        heightAnchor.constraint(equalToConstant: 150).isActive = true
         configureImageView()
+//        configureMealName()
     }
     
     func setUp(with meal: Meal) {
@@ -27,12 +39,21 @@ class MealTableViewCell: UITableViewCell {
     
     private func configureImageView() {
         addSubview(mealImage)
+        mealImage.image = UIImage(named: "Apam balik")
         NSLayoutConstraint.activate([
-            mealImage.topAnchor.constraint(equalTo: topAnchor),
-            mealImage.bottomAnchor.constraint(equalTo: bottomAnchor),
-            mealImage.leftAnchor.constraint(equalTo: leftAnchor),
-            mealImage.rightAnchor.constraint(equalTo: rightAnchor),
-            mealImage.heightAnchor.constraint(equalToConstant: 100)
+            mealImage.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            mealImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            mealImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            mealImage.rightAnchor.constraint(equalTo: rightAnchor, constant: -10)
+        ])
+    }
+    
+    private func configureMealName() {
+        mealImage.addSubview(mealName)
+        mealName.text = "Hello"
+        NSLayoutConstraint.activate([
+            mealName.leftAnchor.constraint(equalTo: mealImage.leftAnchor, constant: 40),
+            mealName.bottomAnchor.constraint(equalTo: mealImage.bottomAnchor, constant: 40)
         ])
     }
     
