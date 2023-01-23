e06b7d486a84748de8b5acf36b556aa69309f3e5
Amogh Joshi <amogh@craftedthoughts.com>
2023-01-23 14:52:32 -0800

    mealImage

--- Fetch Rewards/View/Meals/MealTableViewCell.swift
+++ Fetch Rewards/View/Meals/MealTableViewCell.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
 
+    private lazy var mealImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.textLabel?.text = "Sample"
+        configureImageView()
+    }
+    
+    func setUp(with meal: Meal) {
+        mealImage.image = meal.image
+    }
+    
+    private func configureImageView() {
+        addSubview(mealImage)
+        NSLayoutConstraint.activate([
+            mealImage.topAnchor.constraint(equalTo: topAnchor),
+            mealImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            mealImage.leftAnchor.constraint(equalTo: leftAnchor),
+            mealImage.rightAnchor.constraint(equalTo: rightAnchor),
+            mealImage.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
     
     required init?(coder: NSCoder) {
