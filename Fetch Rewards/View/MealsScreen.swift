b4d091f5ce048346d7ede4aa26f68b8779e3b12e
Amogh Joshi <amogh@craftedthoughts.com>
2023-01-23 13:31:08 -0800

    deleted storyboard related UI, fixed test cases, and added basic programatic UI

---
+++ Fetch Rewards/View/MealsScreen.swift
@@ -0,0 +1,16 @@
+//
+//  MealsScreen.swift
+//  Fetch Rewards
+//
+//  Created by Amogh Joshi on 1/23/23.
+//
+
+import UIKit
+
+class MealsScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
