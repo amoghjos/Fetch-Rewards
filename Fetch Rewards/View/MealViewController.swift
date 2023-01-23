fe648d413fecdfba26feae137300d1c929c17b6f
Amogh Joshi <amogh@craftedthoughts.com>
2023-01-23 13:55:28 -0800

    completed MealViewController

---
+++ Fetch Rewards/View/MealViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MealsScreen.swift
+//  Fetch Rewards
+//
+//  Created by Amogh Joshi on 1/23/23.
+//
+
+import UIKit
+
+class MealViewController: UIViewController {
+    
+    lazy private var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationBar()
+        view.backgroundColor = .systemBackground
+        configureTableView()
+    }
+    
+    private func configureNavigationBar() {
+        title = "Desserts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+        tableView.dataSource = self
+    }
+}
+
+extension MealsScreen: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+}
