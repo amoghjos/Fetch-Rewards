3fc1815494801b07f12c1f294c858811e7cbba2d
Amogh Joshi <amogh@craftedthoughts.com>
2023-01-24 06:53:32 -0800

    setup scroll view

--- Fetch Rewards/View/Meal Detail/MealDetailViewController.swift
+++ Fetch Rewards/View/Meal Detail/MealDetailViewController.swift
@@ -11,6 +11,19 @@ class MealDetailViewController: UIViewController {
 
     var mealID: Int?
     
+    // UI Elements
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+    
     private lazy var mealDetailStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -18,6 +31,7 @@ class MealDetailViewController: UIViewController {
         return stackView
     }()
     
+    // Initializers
     init(with mealID: Int) {
         super.init(nibName: nil, bundle: nil)
         self.mealID = mealID
@@ -27,14 +41,35 @@ class MealDetailViewController: UIViewController {
         super.init(coder: coder)
     }
     
+    //UI Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpScrollView()
         
         guard let mealID = mealID else { return }
         loadMealDetails(for: mealID)
     }
     
+    private func setUpScrollView() {
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+        
+        scrollView.addSubview(contentView)
+        NSLayoutConstraint.activate([
+            contentView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            contentView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            contentView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
+    
+    // Helper
     private func loadMealDetails(for mealID: Int) {
         MealsModelController().getMealDetail(for: mealID) { mealDetail in
             DispatchQueue.main.async {
